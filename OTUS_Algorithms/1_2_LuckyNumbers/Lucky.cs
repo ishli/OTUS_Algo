01cb414ec3be1a9080f1e3752f780c90c9ce0660
Yaroslav Valyalshchikov <yaroslav.valyalshchikov@outlook.com>
2022-05-11 22:41:13 +0300

    1.2 add fast get count of lucky numbers

--- OTUS_Algorithms/1_2_LuckyNumbers/Lucky.cs
+++ OTUS_Algorithms/1_2_LuckyNumbers/Lucky.cs
@@ -13,7 +13,7 @@ namespace LuckyNumbers
 		{
 			var length = Convert.ToInt32(data[0]);
 			count = 0;
-			GetLuckyCountRecursion(length, 0, 0);
+			GetLuckyCountFast(length);
 			return count.ToString();
 		}
 
@@ -36,5 +36,77 @@ namespace LuckyNumbers
 				}
 			}
 		}
-	}
+
+		private void GetLuckyCountFast(int length)
+        {
+			var baseArray = new List<long> { 1 };
+			var result = 0l;
+
+			for (int i = 0; i < length; i++)
+            {
+				var matrix = new long[10, 10*(i+1)];
+				FillMatrixByZero(matrix);
+				FillMatrixByBase(matrix, baseArray);
+				var newBaseArray = GetSumOnEveryColumn(matrix);
+				result = newBaseArray.Select(x => x * x).Sum();
+				baseArray = newBaseArray;
+            }
+
+			count = result;
+        }
+
+        private void FillMatrixByBase(long[,] matrix, List<long> baseArray)
+        {
+			var rowsCount = matrix.GetLength(0);
+			var columnCount = matrix.GetLength(1);
+
+            for (int i = 0; i < rowsCount; i++)
+            {
+                for (int j = 0; j < columnCount; j++)
+                {
+					if(i == j)
+                    {
+                        for (int k = 0; k < baseArray.Count; k++)
+                        {
+							matrix[i, j + k] = baseArray[k];
+                        }
+                    }
+                }
+            }
+		}
+
+        private List<long> GetSumOnEveryColumn(long[,] matrix)
+        {
+			var rowsCount = matrix.GetLength(0);
+			var columnCount = matrix.GetLength(1);
+
+			var result = new List<long>(columnCount);
+
+			for (int i = 0; i < columnCount; i++)
+            {
+				var temp = 0L;
+                for (int j = 0; j < rowsCount; j++)
+                {
+					temp += matrix[j, i];
+                }
+
+				result.Add(temp);
+            }
+
+			return result;
+		}
+
+        private void FillMatrixByZero(long[,] matrix)
+        {
+			var rowsCount = matrix.GetLength(0);
+			var columnCount = matrix.GetLength(1);
+            for (int i = 0; i < rowsCount; i++) 
+            {
+                for (int j = 0; j < columnCount; j++)
+                {
+					matrix[i, j] = 0L;
+                }
+            }
+        }
+    }
 }
